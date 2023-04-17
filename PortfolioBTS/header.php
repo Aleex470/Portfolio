b3dafcdff5f734dfa75685243ec758e3e76397d5
Alexandre <103408282+Aleex470@users.noreply.github.com>
2023-04-17 15:17:40 +0200

    Add files via upload

---
+++ PortfolioBTS/header.php
@@ -0,0 +1,63 @@
+    <header class="header" id="header">
+        <nav class="nav container">
+            <a href="#home" class="nav__logo">Alexandre Visage</a>
+
+            <div class="nav__menu" id="nav-menu">
+                <ul class="nav__list grid">
+                    <li class="nav__item">
+                        <a href="./index.html#home" class="nav__link">
+                            <i class="uil uil-estate nav__icon"></i> Accueil
+                        </a>
+                    </li>
+
+                    <li class="nav__item">
+                        <a href="./index.html#about" class="nav__link">
+                            <i class="uil uil-user nav__icon"></i> Profil
+                        </a>
+                    </li>
+
+                    <li class="nav__item">
+                        <a href="./index.html#skills" class="nav__link">
+                            <i class="uil uil-file-alt nav__icon"></i> Compétences
+                        </a>
+                    </li>
+
+                    <li class="nav__item">
+                        <a href="./index.html#parcours" class="nav__link">
+                            <i class="uil uil-file-alt nav__icon"></i> Parcours
+                        </a>
+                    </li>
+
+                    <li class="nav__item">
+                        <a href="./index.html#veille" class="nav__link">
+                            <i class="uil uil-briefcase-alt nav__icon"></i> Veille
+                        </a>
+                    </li>
+
+                    <li class="nav__item">
+                        <a href="./index.html#projets" class="nav__link">
+                            <i class="uil uil-scenery nav__icon"></i> Projets
+                        </a>
+                    </li>
+
+                    <li class="nav__item">
+                        <a href="./index.html#contact" class="nav__link">
+                            <i class="uil uil-message nav__icon"></i> Contact
+                        </a>
+                    </li>
+                </ul>
+                <i class="uil uil-times nav__close" id="nav-close"></i>
+
+
+            </div>
+            <div class="nav__btns">
+
+                <!-- THEME DARK  -->
+                <i class="uil uil-moon change-theme" onclick="darkTheme(this) ; iconTheme(this)" id="theme-button"></i>
+
+                <div class="nav__toggle" id="nav-toggle">
+                    <i class="uil uil-apps"></i>
+                </div>
+            </div>
+        </nav>
+    </header>
